25d4446ad961726facff60bd1e56df7fe4901c2f
Pawel Idziak <pa.idziak@gmail.com>
2018-11-22 21:23:17 +0100

    added translate service and pipe; adding settings and create test translated text

---
+++ e-tests-web/src/app/shared/translations/lang-en.ts
@@ -0,0 +1,44 @@
+export const LANG_EN_NAME = 'en';
+
+export const LANG_EN_TRANS = {
+
+  // SETTINGS
+  'settings-title': 'Settings',
+  'settings-header': `Change <strong>E-testo</strong> settings`,
+  'settings-paragraph': '2Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+  'language': 'Language',
+  'app-theme': 'App theme',
+  'blue': 'Blue',
+  'light': 'Light',
+  'dark': 'Dark',
+
+  // CREATE-TEST
+  'create-title': 'Create',
+  'create-header': `Let's <strong>create</strong> a new test!`,
+  'create-paragraph': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+  'create-save': 'Save',
+
+  // TEST NAME FORM
+  'test-name-label': 'Test name',
+  'test-name-placeholder': 'Test name...',
+  'test-name-hint': 'Recognizable names are the best!',
+  'test-name-error': 'Test name is required',
+
+  // TEST TAGS FORM
+  'test-tags-label': 'Tags',
+  'test-tags-placeholder': 'New tag...',
+  'test-tags-hint': 'Separate by <strong>ENTER</strong> or <strong>COMMA</strong>',
+  'test-tags-error': 'Choose at least <strong>1</strong> tag',
+
+  // TEST DESC FORM
+  'test-desc-label': 'Description',
+  'test-desc-placeholder': 'Test description...',
+
+  // TEST CREATE FORM
+  'test-create-label': 'Create date',
+
+  // TEST VISIBILITY FORM
+  'test-visibility-public': 'Public',
+  'test-visibility-private': 'Private',
+  'test-visibility-tooltip': 'Private option is unavailable at this moment',
+};
---
+++ e-tests-web/src/app/shared/translations/lang-pl.ts
@@ -0,0 +1,44 @@
+export const LANG_PL_NAME = 'pl';
+
+export const LANG_PL_TRANS = {
+
+  // SETTINGS
+  'settings-title': 'Ustawienia',
+  'settings-header': `Zmień ustawienia <strong>E-testo</strong>`,
+  'settings-paragraph': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+  'language': 'Język',
+  'app-theme': 'Styl aplikacji',
+  'blue': 'Niebieski',
+  'light': 'Jasny',
+  'dark': 'Ciemny',
+
+  // CREATE-TEST
+  'create-title': 'Stwórz',
+  'create-header': `Stwórz <strong>nowy</strong> test!`,
+  'create-paragraph': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+  'create-save': 'Zapisz',
+
+  // TEST NAME FORM
+  'test-name-label': 'Nazwa testu',
+  'test-name-placeholder': 'Nazwa testu...',
+  'test-name-hint': 'Rozpoznawalne nazwy są najlepsze!',
+  'test-name-error': 'Nazwa testu jest wymagana',
+
+  // TEST TAGS FORM
+  'test-tags-label': 'Tagi',
+  'test-tags-placeholder': 'Nowy tag...',
+  'test-tags-hint': 'Oddziel przez <strong>ENTER</strong> lub <strong>PRZECINEK</strong>',
+  'test-tags-error': 'Wybierz co najmniej <strong>1</strong> tag',
+
+  // TEST DESC FORM
+  'test-desc-label': 'Opis',
+  'test-desc-placeholder': 'Opis testu...',
+
+  // TEST CREATE FORM
+  'test-create-label': 'Data utworzenia',
+
+  // TEST VISIBILITY FORM
+  'test-visibility-public': 'Publiczny',
+  'test-visibility-private': 'Prywatny',
+  'test-visibility-tooltip': 'Opcja Prywatny nie jest jeszcze dostępna',
+};
